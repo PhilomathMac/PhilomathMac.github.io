0fa862f1a01feb9970078ad58bffdf8d6a0e3d04
PhilomathMac <93546810+PhilomathMac@users.noreply.github.com>
2022-08-16 09:02:45 -0700

    Update gitprofile.config.js
    Add my personal info

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,93 +2,114 @@
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
+    username: 'PhilomathMac', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
-      forks: false, // Forked projects will not be displayed if set to true.
+      forks: true, // Forked projects will not be displayed if set to true.
       projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
+    linkedin: '',
+    twitter: '',
     facebook: '',
     instagram: '',
     dribbble: '',
     behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
-    website: 'https://arifszn.github.io',
+    medium: '',
+    dev: '',
+    website: 'https://philomathmac.github.io',
     phone: '',
-    email: 'arifulalamszn@gmail.com',
+    email: '',
   },
   skills: [
-    'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
-    'MySQL',
+    'Swift',
+    'SwiftUI',
+    'UIKit',
+    'Java',
+    'Python',
+    'XCode',
+    'Interface Builder',
+    'Version Control',
     'Git',
-    'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
-    'Bootstrap',
+    'GitHub',
+    'App Design'
   ],
   experiences: [
     {
-      company: 'Monstarlab Bangladesh',
-      position: 'Backend Engineer II',
-      from: 'September 2021',
-      to: 'Present',
+      company: 'Pearl Zanker Elementary',
+      position: 'Science Specialist',
+      from: 'Aug 2018',
+      to: 'Jun 2021',
     },
     {
-      company: 'My Offer 360 Degree',
-      position: 'Web Application Developer',
-      from: 'July 2019',
-      to: 'August 2021',
+      company: 'Tyfoom',
+      position: 'Content Specialist',
+      from: 'Sep 2017',
+      to: 'Aug 2018',
     },
+    {
+      company: 'Canyon View Junior High',
+      position: 'Science Teacher',
+      from: 'Aug 2014',
+      to: 'Sep 2017',
+    }
   ],
   education: [
     {
-      institution: 'American International University-Bangladesh',
-      degree: 'Bachelor of Science',
-      from: '2015',
-      to: '2019',
+      institution: 'University of Phoenix',
+      degree: 'Bachelor of Science in Information Technology - Summa Cum Laude',
+      from: 'Jul 2020',
+      to: 'Apr 2022',
     },
     {
-      institution: 'Cantonment College, Jessore',
-      degree: 'Higher Secondary Certificate (HSC)',
-      from: '2012',
-      to: '2014',
+      institution: 'University of Phoenix',
+      degree: 'Advanced Software Developer Certificate - Summa Cum Laude',
+      from: 'Jul 2020',
+      to: 'Apr 2022',
     },
     {
-      institution: 'Chowgacha Shahadat Pilot High School',
-      degree: 'Secondary School Certificate (SSC)',
-      from: '2007',
-      to: '2012',
+      institution: 'Udemy',
+      degree: 'The Git & GitHub Bootcamp',
+      from: 'June 2022',
+      to: 'Aug 2022',
     },
+    {
+      institution: 'Apple',
+      degree: 'App Development with Swift Associate Certificate',
+      from: 'Apr 2022',
+      to: 'Jul 2022',
+    },
+    {
+      institution: 'Apple',
+      degree: 'App Development with Swift Certified User Certificate',
+      from: 'June 2022',
+      to: 'Aug 2022',
+    },
+    {
+      institution: 'Brigham Young University',
+      degree: 'Bachelor of Science in Biological Science Education - Magna Cum Laude',
+      from: 'Aug 2010',
+      to: 'Apr 2014',
+    }
   ],
   // Display blog posts from your medium or dev account. (Optional)
   blog: {
     source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
+    username: '', // to hide blog section, keep it empty
     limit: 3, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
     // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
-    id: 'G-WLLB5E14M6', // Please remove this and use your own tag id or keep it empty
+    id: '', // Please remove this and use your own tag id or keep it empty
   },
   hotjar: {
-    id: '2617601', //  Please remove this and use your own id or keep it empty
+    id: '', //  Please remove this and use your own id or keep it empty
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'corporate',
+    defaultTheme: 'garden',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
@@ -96,39 +117,39 @@ const config = {
 
     // Should use the prefers-color-scheme media-query,
     // using user system preferences, instead of the hardcoded defaultTheme
-    respectPrefersColorScheme: false,
+    respectPrefersColorScheme: true,
 
     // Available themes. To remove any theme, exclude from here.
     themes: [
-      'light',
-      'dark',
-      'cupcake',
-      'bumblebee',
-      'emerald',
-      'corporate',
-      'synthwave',
-      'retro',
-      'cyberpunk',
-      'valentine',
-      'halloween',
+//       'light',
+//       'dark',
+//       'cupcake',
+//       'bumblebee',
+//       'emerald',
+//       'corporate',
+//       'synthwave',
+//       'retro',
+//       'cyberpunk',
+//       'valentine',
+//       'halloween',
       'garden',
       'forest',
-      'aqua',
-      'lofi',
-      'pastel',
-      'fantasy',
-      'wireframe',
-      'black',
-      'luxury',
-      'dracula',
-      'cmyk',
-      'autumn',
-      'business',
-      'acid',
-      'lemonade',
-      'night',
-      'coffee',
-      'winter',
+//       'aqua',
+//       'lofi',
+//       'pastel',
+//       'fantasy',
+//       'wireframe',
+//       'black',
+//       'luxury',
+//       'dracula',
+//       'cmyk',
+//       'autumn',
+//       'business',
+//       'acid',
+//       'lemonade',
+//       'night',
+//       'coffee',
+//       'winter',
       'procyon',
     ],
 
